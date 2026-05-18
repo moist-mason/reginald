56e0fad52ea1833f8a46885ab884fe5041bf7771
moist-mason <67809772+moist-mason@users.noreply.github.com>
2026-05-18 11:42:27 -0500

    init [26.1.2]

--- buildSrc/src/main/groovy/com/github/moistmason/reginald/building/BuildConstants.groovy
+++ buildSrc/src/main/groovy/com/github/moistmason/reginald/building/BuildConstants.groovy
@@ -1,13 +1,13 @@
 package com.github.moistmason.reginald.building
 
 class BuildConstants {
-    public static final def JAVA_VERSION = '21'
-    public static final def MINECRAFT_VERSION = '1.21.1'
+    public static final def JAVA_VERSION = '25'
+    public static final def MINECRAFT_VERSION = '26.1.2'
     public static final def MINECRAFT_VERSION_RANGE = '[' + MINECRAFT_VERSION + ']'
 
-    public static final def NEO_VERSION = '21.1.219'
-    public static final def LOADER_VERSION_RANGE = '[1,)'
-    public static final def PARCHMENT_VERSION = '2024.11.17' // 1.21.1 only
+    public static final def NEO_VERSION = '26.1.2.59-beta'
+    // public static final def LOADER_VERSION_RANGE = '[1,)'
+    // public static final def PARCHMENT_VERSION = '2024.11.17' // 1.21.1 only
 
     public static final def MOD_GROUP = 'com.github.moistmason'
     public static final def MOD_ID = 'reginald'
